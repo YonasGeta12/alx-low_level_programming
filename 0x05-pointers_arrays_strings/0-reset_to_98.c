09a8e072c2a201a7e8af2479bcf000fa27661c9f
Yonas <61965436+YonasGeta12@users.noreply.github.com>
2022-09-20 01:50:08 +0300

    takes a pointer to an int as parameter and updates the value

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,1 +1,12 @@
+#include "main.h"
 
+/**
+ * reset_to_98 - Takes a pointer to an int and updates
+ * the value points to to 98
+ * @n: Pointer n to be updated
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
