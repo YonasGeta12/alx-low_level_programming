9e5b6c8b680bb0e40abade86fb0f1f0f2089bdb5
Yonas <61965436+YonasGeta12@users.noreply.github.com>
2022-09-22 01:15:44 +0300

    a function that copies a string.

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,1 +1,25 @@
+#include "main.h"
 
+/**
+ **_strncpy - Copies a string
+ *@dest: The destination string
+ *@src: The source string
+ *@n: The copy limit
+ *
+ * Return: char value
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a = 0, src_len = 0;
+
+	while (src[a++])
+		src_len++;
+
+	for (a = 0; src[a] && a < n; a++)
+		dest[a] = src[a];
+
+	for (a = src_len; a < n; a++)
+		dest[a] = '\0';
+
+	return (dest);
+}
