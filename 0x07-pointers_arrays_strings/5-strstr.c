c24206cd55c2be01b39987e50c9017d6188166d4
Yonas <61965436+YonasGeta12@users.noreply.github.com>
2022-09-28 00:50:57 +0300

    C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,1 +1,34 @@
+#include "main.h"
 
+/**
+ * _strstr - Finds the first occurrence of the substring
+ * needle in the string haystack.
+ * @haystack: String in which to locate needle.
+ * @needle: Subtring to find in haystack.
+ *
+ * Return: Pointer to the beginning of the located substring
+ * NULL if no substring is found.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i = 0, j = 0;
+
+	while (haystack[i])
+	{
+		while (needle[j] && (haystack[i] == needle[0]))
+		{
+			if (haystack[i + j] == needle[j])
+				j++;
+			else
+				break;
+		}
+		if (needle[j])
+		{
+			i++;
+			j = 0;
+		}
+		else
+			return (haystack + i);
+	}
+	return (0);
+}
