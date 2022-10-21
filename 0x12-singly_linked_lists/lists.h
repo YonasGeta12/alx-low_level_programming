4a9452736ecbc9c48de5fc11b03f3baeac4175d7
Yonas <61965436+YonasGeta12@users.noreply.github.com>
2022-10-21 04:18:31 +0300

    new header file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,19 +1,19 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef _LISTS_
+#define _LISTS_
 
-#include <stddef.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
+ * struct list_s - Singly linked list
+ * @str: string - (malloc string)
  * @len: length of the string
  * @next: points to the next node
  *
- * Description: singly linked list node structure
- * for project
+ * Description: Singly linked list node structure
  */
- typedef struct list_s
+typedef struct list_s
 {
 	char *str;
 	unsigned int len;
@@ -25,5 +25,6 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+void first(void) __attribute__ ((constructor));
 
-#endif /* LISTS_H */
+#endif
